967429cfeaae72887eebe47ecf5c83fb6dc731ac
James Black <james.black.jb2@roche.com>
2019-03-30 20:59:58 +0100

    start example

---
+++ build_readme.R
@@ -0,0 +1,32 @@
+#### This script builds the README.md
+
+library(tidyverse)
+library(glue)
+
+## Params
+
+loc_data <- "talks_table"
+
+## Process
+
+data <- read_csv(loc_data)
+
+## Print
+sink('README.md')
+
+cat("## Building this document\n\n")
+cat("To build this README, run `build_readme.R`. Talks data is in csv `talks_table`\n\n")
+
+cat("## Talks\n\n")
+cat("In alphabetical order.\n\n")
+data %>%
+  arrange(Name) %>%
+  glue_data(
+    "<strong>{Name}, {Affaliation}</strong>; {Title}",
+    "<details><summary>Abstract</summary>",
+    "</p>{Abstract}</p><br>[Slides]({Slides})",
+    "</details><br>"
+  )
+sink()
+
+
